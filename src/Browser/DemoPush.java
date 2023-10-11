b68739eaaf031237a55434ebf8a95cef10e67ab6
SIVARANJANI S <SIVARANJANI S@CSHWLAP000065>
2023-10-11 11:43:00 +0530

    Demo

---
+++ src/Browser/DemoPush.java
@@ -0,0 +1,11 @@
+package Browser;
+
+public class DemoPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Push committed successfully!!!");
+
+	}
+
+}
